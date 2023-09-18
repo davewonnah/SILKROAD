636a5248da04eec4239d0cad6fb1cc8bbde5c1be
Dave Wonnah <wonnahdave@gmail.com>
2023-09-18 12:57:25 +0000

    updating hamburger menu

---
+++ app.js
@@ -0,0 +1,8 @@
+// Get references to the menu elements
+const menuIcon = document.querySelector('.menu i');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+// Add a click event listener to the menu icon
+menuIcon.addEventListener('click', function () {
+    alert("Hello");
+});
